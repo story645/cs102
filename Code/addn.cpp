feba034d6cd526569c75c2c198371f69e18926f8
hannah <story645@gmail.com>
2014-03-07 00:08:34 -0500

    function example

---
+++ Code/addn.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int addnv(int x, int n=0);
+void addnr(int &x, int n=0);
+
+int main(){
+	int a=5;
+	cout<<"Pass-by-value"<<endl;
+	cout<<"a="<<a<<endl;
+	cout<<addnv(a)<<endl;
+	cout<<"a="<<a<<endl;
+	cout<<addnv(a, 10)<<endl;
+	cout<<"a="<<a<<endl;
+	cout<<addnv(a, 15)<<endl;
+	cout<<"a="<<a<<endl;
+	cout<<"Pass-by-reference"<<endl;
+	cout<<"a="<<a<<endl;
+	addnr(a);
+	cout<<"a="<<a<<endl;
+	addnr(a, 10);
+	cout<<"a="<<a<<endl;
+	addnr(a, 15);
+	cout<<"a="<<a<<endl;
+	
+	return 0;
+}
+
+//adds n to x, pass by value
+int addnv(int x, int n=0){
+	return x+n;
+}
+
+//adds n to x, pass by reference
+void addnr(int &x, int n=0){
+	x = x+n;
+}
