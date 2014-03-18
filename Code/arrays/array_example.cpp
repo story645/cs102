c33bfcd0724debf5f7e8585d063a779518d62c56
hannah <story645@gmail.com>
2014-03-18 02:20:12 -0400

    more stuff into folders

---
+++ Code/arrays/array_example.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	const int LENGTH = 5;
+	int arr[LENGTH]={2,4,6,8,10};
+	for(int i=0; 
+	i<=2*LENGTH; 
+	i++){
+		cout<<arr[i]<<endl;
+	}
+return 0;
+}
