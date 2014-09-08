79373a80cbaecdf9a9f257af70ccf391aeaeb656
hannah <story645@gmail.com>
2014-09-08 19:08:35 -0400

    assignments lecture and datatypes and helloworld handouts

---
+++ Code/assignments/testptr.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int x = 10;
+	int *y = &x;
+	cout<<x<<" "<<*y<<endl;
+	x = 20;
+	cout<<x<<" "<<*y<<endl;
+	*y = 5;
+	cout<<x<<" "<<*y<<endl;
+
+}
