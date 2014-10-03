e99a1ab4f408fc1036a0a13f978c039a02d4e8a9
hannah <story645@gmail.com>
2014-10-03 16:43:49 -0400

    loops problem sets

---
+++ Code/loops/pretzloop.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+   char pretz='y';
+   int dough = 40;
+   
+   if(while(pretz=='y' && dough>0))
+     {
+	
+	cout<<"Grab each end of dough rope"<<endl;
+	cout<<"Make a U-Shape with the rope"<<endl;
+	cout<<"Hold the ends of the rope"<<endl;
+	cout<<"Cross the ends over each other"<<endl;
+	cout<<"Press onto the bottom of the U"<<endl;
+	dough-=10;
+	cout<<"Want another pretzle? ";
+	cin>>pretz;
+	};
+return 0;
+}
