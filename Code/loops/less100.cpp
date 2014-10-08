ac2eac70772906c7c05d8fd008d92d662593262b
hannah <story645@gmail.com>
2014-10-08 15:01:28 -0400

    fixed loop recitation bug

---
+++ Code/loops/less100.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int x = 102;
+   while(x>100)
+     {
+	cout<<"Enter an integer (x>100): ";
+	cin>>x;
+     }   
+   
+   return 0;
+}
---
+++ Code/loops/less100do.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int x;
+   
+   do
+     {
+	cout<<"Enter an integer (x>100): ";
+	cin>>x;
+     }while(x>100);   
+   
+   return 0;
+}
